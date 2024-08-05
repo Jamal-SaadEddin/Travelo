34313ff0addeac07108514cf2917cc6494e6cbb2
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-08-05 14:55:08 +0300

    Create Login Page #8

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,102 @@
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Hidden, Paper } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import PromotionalSection from "../components/PromotionalSection";
+
+const LoginPage = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Grid
+      container
+      component="main"
+      sx={{ height: "100vh", alignContent: "center", justifyContent: "center" }}
+    >
+      <Grid item xs={0} md={7} lg={6}>
+        <Hidden mdDown>
+          <PromotionalSection />
+        </Hidden>
+      </Grid>
+
+      <Grid
+        item
+        xs={11}
+        sm={10}
+        md={4}
+        lg={4}
+        component={Paper}
+        elevation={6}
+        square
+        alignContent={"center"}
+      >
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            // justifyContent: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default LoginPage;
