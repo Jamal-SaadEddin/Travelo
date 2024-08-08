29adf818439522d5948214bae284845de98e4441
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-08-08 20:16:44 +0300

    2.1 Robust Search Functionality #14
    - Add date picker component from mui-x library
    - Add Room Booking Selector component
    - Add Search button
    - All UIs, no behaviour implementation

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,9 +1,14 @@
-import { Box, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { Box, Button, InputAdornment, TextField } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import dayjs from "dayjs";
+import RoomBookingSelector from "./RoomBookingSelector/RoomBookingSelector";
 
 const styles = {
   searchBox: {
     display: "flex",
-    flexWrap: "wrap",
     gap: 2,
     justifyContent: "space-between",
     p: 2,
@@ -15,9 +20,52 @@ const styles = {
 
 const SearchBox = () => {
   return (
-    <Box sx={styles.searchBox}>
-      <TextField placeholder="Search for hotels, cities..." />
-    </Box>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box sx={styles.searchBox} flexDirection={{ xs: "column", md: "row" }}>
+        <Box width={{ xs: "100%", md: "25%" }}>
+          <TextField
+            placeholder="Search for hotels, cities..."
+            variant="outlined"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <Box width={{ xs: "100%", md: "35%" }} display="flex" gap={1}>
+          <DatePicker
+            label="Check-in"
+            defaultValue={dayjs()}
+            minDate={dayjs()}
+            sx={{ width: "50%" }}
+          />
+          <DatePicker
+            label="Check-out"
+            defaultValue={dayjs().add(1, "day")}
+            minDate={dayjs().add(1, "day")}
+            sx={{ width: "50%" }}
+          />
+        </Box>
+        <Box width={{ xs: "100%", md: "25%" }}>
+          <RoomBookingSelector
+            roomInfo={{ adults: 2, children: 0, rooms: 1 }}
+          />
+        </Box>
+        <Box width={{ xs: "100%", md: "15%" }}>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ height: "100%", fontWeight: "bold", fontSize: "16px" }}
+          >
+            Search
+          </Button>
+        </Box>
+      </Box>
+    </LocalizationProvider>
   );
 };
 
