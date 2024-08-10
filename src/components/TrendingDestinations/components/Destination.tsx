5b1b1e7966be7da2ed768c0d13526c349bbb5ec4
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-08-10 19:29:53 +0300

    Extract trendingDestinationStyles css

--- src/components/TrendingDestinations/components/Destination.tsx
+++ src/components/TrendingDestinations/components/Destination.tsx
@@ -1,40 +1,17 @@
 import { Box, CardMedia, Typography } from "@mui/material";
+import { trendingDestinationStyles } from "../../../styles";
 import { DestinationProps } from "../entities/DestinationProps";
-
-const styles = {
-  styledCard: {
-    overflow: "hidden",
-    position: "relative",
-    width: "100%",
-    height: "100%",
-  },
-  styledCardMedia: {
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-    objectPosition: "top",
-    borderRadius: "5px",
-  },
-  cityName: {
-    position: "absolute",
-    top: "16px",
-    left: "16px",
-    color: "white",
-    textShadow: "0px 0px 8px rgba(0, 0, 0, 0.5)",
-    fontWeight: "bold",
-  },
-};
 
 const Destination = ({ destination }: DestinationProps) => {
   return (
-    <Box sx={styles.styledCard}>
+    <Box sx={trendingDestinationStyles.styledCard}>
       <CardMedia
         component="img"
         src={destination.thumbnailUrl}
         alt={destination.cityName}
-        sx={styles.styledCardMedia}
+        sx={trendingDestinationStyles.styledCardMedia}
       />
-      <Typography variant="h5" sx={styles.cityName}>
+      <Typography variant="h5" sx={trendingDestinationStyles.cityName}>
         {destination.cityName}
       </Typography>
     </Box>
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -27,3 +27,27 @@ export const featuredDealHotelCardWidth = {
   md: "calc(33.333% - 1.333rem)",
   lg: "calc(25% - 1.5rem)",
 };
+
+export const trendingDestinationStyles = {
+  styledCard: {
+    overflow: "hidden",
+    position: "relative",
+    width: "100%",
+    height: "100%",
+  },
+  styledCardMedia: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    objectPosition: "top",
+    borderRadius: "5px",
+  },
+  cityName: {
+    position: "absolute",
+    top: "16px",
+    left: "16px",
+    color: "white",
+    textShadow: "0px 0px 8px rgba(0, 0, 0, 0.5)",
+    fontWeight: "bold",
+  },
+};
