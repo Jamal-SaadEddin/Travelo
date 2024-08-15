b8409f610ffff0798cfa671bdc7495f4fd93752c
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-08-15 16:15:38 +0300

    Add all reviews drawer to view all customers reviews

--- src/components/ReviewsSlider/components/Header.tsx
+++ src/components/ReviewsSlider/components/Header.tsx
@@ -1,4 +1,15 @@
-import { Button, Stack, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  Button,
+  Drawer,
+  IconButton,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { reviews } from "../constants/reviews";
+import ReviewItem from "./ReviewItem";
 
 const styles = {
   textTransform: "none",
@@ -11,6 +22,12 @@ const styles = {
 };
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
   return (
     <Stack
       direction="row"
@@ -19,7 +36,31 @@ const Header = () => {
       mb={1}
     >
       <Typography variant="h4">Reviews</Typography>
-      <Button sx={styles}>View all</Button>
+      <Button sx={styles} onClick={toggleDrawer(true)}>
+        View all
+      </Button>
+      <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+        <Stack sx={{ p: 2 }}>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            mb={2}
+          >
+            <Typography variant="h4">All Reviews</Typography>
+            <IconButton onClick={toggleDrawer(false)}>
+              <CloseIcon />
+            </IconButton>
+          </Stack>
+          <Stack spacing={2}>
+            {reviews.map((review) => (
+              <Paper sx={{ maxWidth: "408px" }}>
+                <ReviewItem key={review.reviewId} review={review} />
+              </Paper>
+            ))}
+          </Stack>
+        </Stack>
+      </Drawer>
     </Stack>
   );
 };
