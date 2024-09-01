edd623fd3c264bcc565ebb5fb3474ec6d137d6dd
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-09-01 13:38:49 +0300

    Edit routes
    - Ensure admin can only access his own routes
    - Ensure user can only access his own routes
    - Ensure non-user is navigated always to LoginPage

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import RootRoute from "./RootRoute";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import { RootChildren } from "./RootChildren";
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />}>
-          <Route index element={<RootRoute />} />
-        </Route>
+        <Route path="/" element={<HomePage />} children={RootChildren()} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/not-found" />} />
+        <Route path="/not-found" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
