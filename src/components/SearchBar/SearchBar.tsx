808ae7c96fc5c7246d7e8cb845989b932218428d
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-09-08 11:43:55 +0300

    Render searchBar id & placeholder dynamically

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,37 @@
 import SearchIcon from "@mui/icons-material/Search";
 import { InputAdornment, TextField } from "@mui/material";
 import { useState } from "react";
+import useCurrentPageStore from "../../store/currentPage.store";
 
 const SearchBar = () => {
   const [searchText, setSearchText] = useState("");
+  const pageData = useCurrentPageStore((state) => state.currentPage);
+  let id;
+  let placeholder;
+
+  switch (pageData) {
+    case "cities":
+      id = "search-cities";
+      placeholder = "Search for cities...";
+      break;
+    case "hotels":
+      id = "search-hotels";
+      placeholder = "Search for hotels...";
+      break;
+    case "rooms":
+      id = "search-rooms";
+      placeholder = "Search for rooms...";
+      break;
+    default:
+      id = "search";
+      placeholder = "Search...";
+      break;
+  }
 
   return (
     <TextField
-      id="cityName" // changable
-      placeholder="Search for cities..." // changable
+      id={id}
+      placeholder={placeholder}
       variant="outlined"
       InputProps={{
         startAdornment: (
