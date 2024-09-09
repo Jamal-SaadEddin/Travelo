d3085737e18b3a7424ee2fb386a0b329ddcd5564
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-09-09 10:42:40 +0300

    Add useRecentlyVisitedHotels custom hook

--- src/components/RecentlyVisitedHotels/RecentlyVisitedHotels.tsx
+++ src/components/RecentlyVisitedHotels/RecentlyVisitedHotels.tsx
@@ -1,16 +1,19 @@
 import { Stack } from "@mui/material";
+import { useRecentlyVisitedHotels } from "../../hooks/useRecentlyVisitedHotels";
 import { useStyles } from "../../styles";
 import HotelCard from "../common/HotelCard";
-import { hotels } from "./constants/hotels";
 
 const RecentlyVisitedHotels = () => {
   const { hotelCardsStyles } = useStyles();
+  const { data: hotels, isLoading } = useRecentlyVisitedHotels();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <Stack direction="row" sx={hotelCardsStyles.cardsStackStyles}>
-      {hotels.map((hotel) => (
-        <HotelCard key={hotel.hotelId} hotel={hotel} />
-      ))}
+      {hotels?.map((hotel) => <HotelCard key={hotel.hotelId} hotel={hotel} />)}
     </Stack>
   );
 };
---
+++ src/hooks/useRecentlyVisitedHotels.ts
@@ -0,0 +1,22 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { Hotel } from "../components/common/HotelCard/entities/Hotel";
+import { createApiClient } from "../services/createApiClient";
+import useAuthStore from "../store/auth.store";
+
+const apiClient = createApiClient();
+
+export const useRecentlyVisitedHotels = () => {
+  const user = useAuthStore((state) => state.user);
+
+  return useQuery<Hotel[], Error>({
+    queryKey: ["recently-visited-hotels"],
+    queryFn: async (): Promise<Hotel[]> => {
+      const response = await apiClient.api.homeUsersRecentHotelsDetail(
+        Number(user?.user_id),
+      );
+      return response.data as Hotel[];
+    },
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    placeholderData: keepPreviousData,
+  });
+};
